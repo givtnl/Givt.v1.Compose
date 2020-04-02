88c9ed462371eb0eae087fe044d3b2701ec382eb
Maarten Vergouwe <mvergouwe85@gmail.com>
2020-04-02 11:27:54 +0200

    Improve base-dotnet-image

--- azure-dotnet-image/docker/init_container.sh
+++ azure-dotnet-image/docker/init_container.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+# Get environment variables to show up in SSH session
+eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
+
+# start ssh service
 service ssh start
 
 [ -z "$ASPNETCORE_URLS" ] && export ASPNETCORE_URLS=http://*:"$PORT"
 
 cd /home/site/wwwroot/
+[ -e "$DLL" ] && exec dotnet "$DLL"
 
-exec dotnet hostingstart.dll
+echo Could not find .dll
+
+exec /bin/bash
