b2d91abbe98dae13107509b31fbf38f02745c809
Mike <mike@givtapp.net>
2022-03-14 16:03:26 +0100

    Added a version of the nginx image for azure that has specific settings to handle browser's accept language header for angular internationalization

---
+++ azure-nginx-image-angular/docker/init_container.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Get environment variables to show up in SSH session
+eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
+
+# Starting nginx
+mkdir -p /home/LogFiles/nginx
+# this line is required or else it will error a while when running ze container
+if test ! -e /home/LogFiles/nginx/error.log; then 
+    touch /home/LogFiles/nginx/error.log
+fi
+
+#start ssh service
+/usr/sbin/sshd
+
+#disable the can't open /dev/tty1: No such file or directory spam sausages
+ln -sf /dev/null /dev/tty1
+ln -sf /dev/null /dev/tty2
+ln -sf /dev/null /dev/tty3
+ln -sf /dev/null /dev/tty4
+ln -sf /dev/null /dev/tty5
+ln -sf /dev/null /dev/tty6
+
+exec /usr/sbin/nginx 
